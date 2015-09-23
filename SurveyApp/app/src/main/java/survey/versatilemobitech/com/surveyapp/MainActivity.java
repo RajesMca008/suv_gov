4e3b88a387609f2f57652e47686da8cfaf16bd11
RajesMca008 <RajesMca008@users.noreply.github.com>
2015-09-24 00:23:34 +0530

    First commit

---
+++ SurveyApp/app/src/main/java/survey/versatilemobitech/com/surveyapp/MainActivity.java
@@ -0,0 +1,16 @@
+package survey.versatilemobitech.com.surveyapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.completed_details);
+
+
+    }
+
+}
